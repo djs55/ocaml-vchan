751308f3dd8282f44b3ccdf94dbbd5674b7400b8
David Scott <dave.scott@citrix.com>
2014-12-01 22:03:28 +0000

    Add a Unix_configuration module
    Instead of xenstore we "write" the configuration to environment
    variables.
    
    Signed-off-by: David Scott <dave.scott@citrix.com>

---
+++ lib/unix_configuration.ml
@@ -0,0 +1,37 @@
+(*
+ * Copyright (C) Citrix Systems Inc.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+open Sexplib.Std
+open Lwt
+
+(* FIXME: This should probably be pushed into xen-evtchn *)
+
+type t = {
+  ring_ref: string;
+  event_channel: string;
+} with sexp
+
+let _env_var = "XEN_CONFIGURATION"
+
+let write ~client_domid ~port t =
+  Printf.fprintf stderr "%s=\"%s\"\n%!" _env_var (String.escaped (Sexplib.Sexp.to_string_hum (sexp_of_t t)));
+  return ()
+
+let read ~server_domid ~port =
+  return (t_of_sexp (Sexplib.Sexp.of_string (Sys.getenv _env_var)))
+
+let delete ~client_domid ~port =
+  return ()
